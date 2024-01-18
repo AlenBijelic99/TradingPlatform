0a2a30e284f1ab41a6c9447182f401e078fe74db
TegestBogale <tegest.g.bogale@gmail.com>
2024-01-18 20:30:45 +0100

    fixed paths & renamed pages

---
+++ frontend/views/home/HomeView.tsx
@@ -0,0 +1,33 @@
+import {Button} from '@hilla/react-components/Button.js';
+import {Notification} from '@hilla/react-components/Notification.js';
+import {TextField} from '@hilla/react-components/TextField.js';
+import {Grid} from "@hilla/react-components/Grid";
+import {GridColumn} from "@hilla/react-components/GridColumn";
+import {CryptoCurrencyService} from 'Frontend/generated/endpoints.js';
+import {useEffect, useState} from 'react';
+import CryptoCurrencyRecord
+    from "Frontend/generated/ch/heigvd/application/services/CryptoCurrencyService/CryptoCurrencyRecord";
+
+export default function HomeView() {
+    const [cryptoCurrencies, setCryptoCurrencies] = useState<CryptoCurrencyRecord[]>([]);
+
+    useEffect(() => {
+        CryptoCurrencyService.getAllWithPrice().then(setCryptoCurrencies);
+    });
+
+    return (
+        <>
+            <section>
+                <div className="p-m flex gap-m">
+                    <Grid
+                        items={cryptoCurrencies}>
+
+                        <GridColumn path="name"/>
+                        <GridColumn path="symbol"/>
+                        <GridColumn path="price"/>
+                    </Grid>
+                </div>
+            </section>
+        </>
+    );
+}
--- frontend/views/trading/tradingView.tsx
+++ frontend/views/trading/tradingView.tsx
@@ -1,4 +1,4 @@
-export default function AboutView() {
+export default function TradingView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
       <img style={{ width: '200px' }} src="images/empty-plant.png" />
