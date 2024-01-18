27f8645b7e5a30a7536fd5e8cc667b98e58c8b5c
TegestBogale <tegest.g.bogale@gmail.com>
2024-01-18 20:33:45 +0100

    Added signup views to routes & renamed others

--- frontend/routes.tsx
+++ frontend/routes.tsx
@@ -1,22 +1,27 @@
-import { protectRoutes } from '@hilla/react-auth';
-import HelloWorldView from 'Frontend/views/helloworld/HelloWorldView.js';
+import {protectRoutes} from '@hilla/react-auth';
+import HomeView from 'Frontend/views/home/HomeView';
 import LoginView from 'Frontend/views/login/LoginView.js';
 import MainLayout from 'Frontend/views/MainLayout.js';
-import { lazy } from 'react';
-import { createBrowserRouter, RouteObject } from 'react-router-dom';
+import {lazy} from 'react';
+import {createBrowserRouter, RouteObject} from 'react-router-dom';
+import SignUpView from "Frontend/views/signUp/SignUpView";
+import TradingView from "Frontend/views/trading/tradingView";
 
-const AboutView = lazy(async () => import('Frontend/views/about/AboutView.js'));
+const AboutView = lazy(async () => import('Frontend/views/trading/tradingView'));
 
 export const routes = protectRoutes([
-  {
-    element: <MainLayout />,
-    handle: { title: 'Main' },
-    children: [
-      { path: '/', element: <HelloWorldView />, handle: { title: 'Hello World', requiresLogin: true } },
-      { path: '/about', element: <AboutView />, handle: { title: 'About', requiresLogin: true } },
-    ],
-  },
-  { path: '/login', element: <LoginView /> },
+    {path: '/', element: <SignUpView/>},
+    {path: '/login', element: <LoginView/>},
+    {
+        element: <MainLayout/>,
+        handle: {title: 'Main'},
+        children: [
+            {path: '/home', element: <HomeView/>, handle: {title: 'Home', requiresLogin: true}},
+            {path: '/trade', element: <TradingView/>, handle: {title: 'Trade', requiresLogin: true}},
+        ],
+    },
+
+
 ]) as RouteObject[];
 
 export default createBrowserRouter(routes);
