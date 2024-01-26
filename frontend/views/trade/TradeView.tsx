284c28ba80f8efbd608c3081f3c6624c80579a10
Alen Bijelic <alen.bijelic@heig-vd.ch>
2024-01-26 17:34:51 +0100

    WIP Enhancing trading

--- frontend/views/trade/TradeView.tsx
+++ frontend/views/trade/TradeView.tsx
@@ -1,26 +1,34 @@
 import {CryptoCurrencyService, TradeService} from 'Frontend/generated/endpoints.js';
-import CryptoCurrencyRecord
-    from "Frontend/generated/ch/heigvd/application/services/CryptoCurrencyService/CryptoCurrencyRecord";
 import {GridColumn} from "@hilla/react-components/GridColumn";
 import {Grid} from "@hilla/react-components/Grid";
-import {useEffect, useState} from 'react';
-import {Dialog} from '@hilla/react-components/Dialog.js';
-import {Button} from "@hilla/react-components/Button.js";
-import {VerticalLayout} from "@hilla/react-components/VerticalLayout";
+import React, {SetStateAction, useEffect, useState} from 'react';
+import CryptoCurrency from "Frontend/generated/ch/heigvd/application/data/entities/CryptoCurrency";
+import {Notification} from '@hilla/react-components/Notification.js';
+import {HorizontalLayout} from '@hilla/react-components/HorizontalLayout.js';
 import {NumberField} from "@hilla/react-components/NumberField";
-import {Notification} from "@hilla/react-components/Notification";
-import {EndpointError} from "@hilla/frontend";
+import {RadioGroup} from "@hilla/react-components/RadioGroup";
+import {RadioButton} from "@hilla/react-components/RadioButton";
+import {Button} from '@hilla/react-components/Button.js';
 
 export default function TradeView() {
-    const [cryptoCurrencies, setCryptoCurrencies] = useState<CryptoCurrencyRecord[]>([]);
-    const [selectedItem, setSelectedItem] = useState<CryptoCurrencyRecord | null>(null);
-    const [dialogOpened, setDialogOpened] = useState(false);
-    const [selectedAction, setSelectedAction] = useState<'Buy' | 'Sell'>('Buy');
-    const [tradeAmount, setTradeAmount] = useState<number>(0);
+    const [cryptoCurrencies, setCryptoCurrencies] = useState<CryptoCurrency[]>([]);
+    const [selectedItem, setSelectedItem] = useState<CryptoCurrency | null>(null);
+    const [usdAmound, setUsdAmound] = useState("0");
+    const [cryptoAmount, setCryptoAmount] = useState("0");
+    const [tradeAction, setTradeAction] = useState<'buy' | 'sell'>('buy');
 
     const loadCryptoCurrencies = () => {
         CryptoCurrencyService.getAllWithPrice().then(setCryptoCurrencies);
     }
+
+    useEffect(() => {
+        if (selectedItem && usdAmound) {
+            const amountInUSD = parseFloat(usdAmound);
+            const cryptoPrice = selectedItem.lastPrice;
+            const amountInCrypto = amountInUSD / cryptoPrice;
+            setCryptoAmount(amountInCrypto.toFixed(8));
+        }
+    }, [selectedItem, usdAmound]);
 
     useEffect(() => {
         loadCryptoCurrencies();
@@ -32,44 +40,38 @@ export default function TradeView() {
         return () => clearInterval(interval);
     }, []);
 
-    const resetState = () => {
-        // Reset state after successful trade
-        setDialogOpened(false);
-        setSelectedItem(null);
-        setSelectedAction('Buy');
-        setTradeAmount(0);
-    }
-    const handleTrade = async () => {
-
-        try {
-            if (!selectedItem) {
-                //Just to be sure for the compiler that selectedItem is not null. Since
-                // the dialog is opened only if selectedItem is not null, this should never happen
-                return;
-            }
-            // Call the TradeService to perform the buy/sell operation
-            if (selectedAction === 'Buy') {
-                console.log("waiting for TradeService.buy");
-
-                await TradeService.buy(selectedItem.symbol, tradeAmount);
-            } else if (selectedAction === 'Sell') {
-                console.log("waiting for TradeService.sell");
-                await TradeService.sell(selectedItem.symbol, tradeAmount);
-            }
-            resetState();
-        } catch (error) {
-            if (error instanceof EndpointError) {
-                Notification.show((error as EndpointError).message, {theme: 'error'});
-            } else {
-                Notification.show("Unexpected error", {theme: 'error'});
+    useEffect(() => {
+        if (selectedItem) {
+            const updatedItem = cryptoCurrencies.find(crypto => crypto.symbol === selectedItem.symbol);
+            if (updatedItem) {
+                setSelectedItem(updatedItem);
             }
         }
+    }, [cryptoCurrencies]);
+
+
+    const handleTrade = () => {
+        const amount = parseFloat(usdAmound);
+        if (selectedItem && selectedItem.symbol && amount > 0) {
+            const tradeFunction = tradeAction === 'buy' ? TradeService.buy : TradeService.sell;
+            tradeFunction(selectedItem.symbol, amount)
+                .then(() => {
+                    Notification.show(tradeAction === 'buy' ? 'Achat effectué' : 'Vente effectuée');
+                    setUsdAmound("0");
+                })
+                .catch((error) => Notification.show(`Erreur lors du trade: ${error.message}`));
+        }
+    }
+
+    const handleBuyClick = () => {
+        setTradeAction('buy');
     };
 
+    const handleSellClick = () => {
+        setTradeAction('sell');
+    };
 
-    const close = () => {
-        resetState();
-    }
+    const buttonTheme = `primary ${tradeAction === 'buy' ? 'success' : 'error'}`;
 
     return (
         <>
@@ -81,67 +83,38 @@ export default function TradeView() {
                             const item = e.detail.value;
                             setSelectedItem(item || null);
                             if (item) {
-                                setDialogOpened(true);
+                                //setDialogOpened(true);
                             }
                         }}
                     >
                         <GridColumn path="name"/>
                         <GridColumn path="symbol"/>
-                        <GridColumn path="price"/>
+                        <GridColumn path="lastPrice"/>
                     </Grid>
+                    {selectedItem && (
+                        <div>
+                            <h3>{selectedItem.name}</h3>
+                            <h3>{selectedItem.lastPrice}</h3>
+                            <RadioGroup label="Action" value={tradeAction}>
+                                <RadioButton label="Buy" value="buy" onClick={handleBuyClick}/>
+                                <RadioButton label="Sell" value="sell" onClick={handleSellClick}/>
+                            </RadioGroup>
+                            <HorizontalLayout theme="spacing">
+                                <NumberField label="USD Amount" value={usdAmound.toString()}
+                                             onChange={e => setUsdAmound(e.target.value)}>
+                                    <div slot="prefix">$</div>
+                                </NumberField>
+                                <NumberField label={`${selectedItem.symbol} Amount`} value={cryptoAmount} readonly>
+                                    <div slot="prefix">{selectedItem.symbol}</div>
+                                </NumberField>
+                            </HorizontalLayout>
+                            <Button onClick={handleTrade} theme={buttonTheme}>
+                                {tradeAction === 'buy' ? 'Buy' : 'Sell'}
+                            </Button>
+                        </div>
+                    )}
                 </div>
             </section>
-
-            {selectedItem && (
-                <Dialog
-                    aria-label={`Trade ${selectedItem.name}`}
-                    modeless={false}
-                    opened={dialogOpened}
-                    onOpenedChanged={(event) => setDialogOpened(event.detail.value)}
-                    headerRenderer={() => (
-                        <h2
-                            className="draggable"
-                            style={{
-                                flex: 1,
-                                cursor: 'move',
-                                margin: 0,
-                                fontSize: '1.5em',
-                                fontWeight: 'bold',
-                                padding: 'var(--lumo-space-m) 0',
-                            }}
-                        >
-                            {selectedAction === 'Buy' ? `Buy ${selectedItem.name}` : `Sell ${selectedItem.name}`}
-                        </h2>
-                    )}
-                    footerRenderer={() => (
-                        <>
-                            <Button onClick={handleTrade}>Trade</Button>
-                            <Button onClick={close}> Cancel </Button>
-
-                        </>
-                    )}
-                >
-                    <VerticalLayout theme="spacing">
-                        <select
-                            value={selectedAction || ''}
-                            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setSelectedAction(event.target.value as 'Buy' | 'Sell')}
-                        >
-                            <option value="Buy">Buy</option>
-                            <option value="Sell">Sell</option>
-                        </select>
-
-                        <NumberField
-                            label="Amount"
-                            value={tradeAmount !== null ? tradeAmount.toString() : undefined}
-                            onValueChanged={(event) =>
-                                setTradeAmount(event.detail.value !== null ? parseFloat(event.detail.value) : 0)
-                            }
-                        />
-
-
-                    </VerticalLayout>
-                </Dialog>
-            )}
         </>
     );
 }
